57214b864f6d959ee9df4a703e6b51ec4bf270bb
Carl Oscar Aaro <hello@carloscar.com>
2019-12-02 19:24:17 +0100

    Added additional test cases

--- tests/test_class_extension.py
+++ tests/test_class_extension.py
@@ -12,6 +12,15 @@ def test_class_extension():
         def _test(self, value):
             return value
 
+        @staticmethod
+        def _staticmethod_test(value):
+            return value
+
+        @classmethod
+        def _classmethod_test(cls, value):
+            cls.data = value
+            return (cls, value)
+
     with pytest.raises(AttributeError):
         testdecorator.__version__
 
@@ -34,11 +43,18 @@ def test_class_extension():
     with pytest.raises(TypeError):
         testdecorator._test(5)
 
+    assert testdecorator._staticmethod_test(5) == 5
+    assert testdecorator._classmethod_test(5) == (testdecorator, 5)
+    assert testdecorator.data == 5
+
     d = testdecorator()
     assert d._test
     with pytest.raises(TypeError):
         d._test()
     assert d._test(5) == 5
+
+    assert d._staticmethod_test(5) == 5
+    assert d._classmethod_test(5) == (d.__class__, 5)
 
     d = testdecorator.decorator()
     assert d._test
@@ -46,9 +62,20 @@ def test_class_extension():
         d._test()
     assert d._test(5) == 5
 
+    assert d._staticmethod_test(5) == 5
+    assert d._classmethod_test(5) == (d.__class__, 5)
+
     class anotherdecorator(testdecorator):
         def _other_test(self, value):
             return value * 2
+
+        @staticmethod
+        def _other_staticmethod_test(value):
+            return value * 2
+
+        @classmethod
+        def _other_classmethod_test(cls, value):
+            return (cls, testdecorator, value * 2)
 
     assert anotherdecorator.__author__
 
@@ -62,6 +89,12 @@ def test_class_extension():
     with pytest.raises(TypeError):
         anotherdecorator._test(5)
 
+    assert anotherdecorator._staticmethod_test(5) == 5
+    assert anotherdecorator._classmethod_test(20) == (anotherdecorator, 20)
+    assert anotherdecorator._other_staticmethod_test(5) == 10
+    assert anotherdecorator._other_classmethod_test(5) == (anotherdecorator, testdecorator, 10)
+    assert anotherdecorator.data == 20
+
     d = anotherdecorator()
     assert d._test
     with pytest.raises(TypeError):
@@ -69,9 +102,19 @@ def test_class_extension():
     assert d._test(5) == 5
     assert d._other_test(5) == 10
 
+    assert d._staticmethod_test(5) == 5
+    assert d._classmethod_test(5) == (d.__class__, 5)
+    assert d._other_staticmethod_test(5) == 10
+    assert d._other_classmethod_test(5) == (d.__class__, testdecorator, 10)
+
     d = anotherdecorator.decorator()
     assert d._test
     with pytest.raises(TypeError):
         d._test()
     assert d._test(5) == 5
     assert d._other_test(5) == 10
+
+    assert d._staticmethod_test(5) == 5
+    assert d._classmethod_test(5) == (d.__class__, 5)
+    assert d._other_staticmethod_test(5) == 10
+    assert d._other_classmethod_test(5) == (d.__class__, testdecorator, 10)
